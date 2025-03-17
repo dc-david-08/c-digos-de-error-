55a9a3bc501dc432123fbe10e1546bff18e12ccf
afmirandadiaz <amirandadiaz@godaddy.com>
2025-03-17 10:09:44 -0500

    First Commit

--- regexCoders.py
+++ regexCoders.py
@@ -1,1 +1,10 @@
 import requests, json, re
+
+def captureData(url):
+    try:
+        respuesta = requests.get(url).json()
+        regexExpresion = "^[a-zA-Z]+$"
+        regex = re.findall(respuesta, regexExpresion)
+    except Exception as e:
+        respuesta = None
+
