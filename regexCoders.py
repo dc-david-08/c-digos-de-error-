fd228adb97c78d41e03d6930308c0afbefb1a77a
afmirandadiaz <amirandadiaz@godaddy.com>
2025-03-17 10:18:02 -0500

    First Commit

--- regexCoders.py
+++ regexCoders.py
@@ -3,8 +3,13 @@ import requests, json, re
 def captureData(url):
     try:
         respuesta = requests.get(url).json()
-        regexExpresion = "^[a-zA-Z]+$"
+        regexExpresion = "^(\d{3}\.\d{3}\.\d{3}\.\d{3}).*?\"\s(\d{3}).*?$"
         regex = re.findall(respuesta, regexExpresion)
+        return regex
     except Exception as e:
-        respuesta = None
+        return None
+
+print(captureData("https://github.com/elastic/examples/raw/refs/heads/master/Common%20Data%20Formats/apache_logs/apache_logs"))
+
+
 
