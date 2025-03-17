d7c73c2f28f7ce3d5c61cc472696ce0651ebe474
afmirandadiaz <amirandadiaz@godaddy.com>
2025-03-17 10:28:32 -0500

    First Commit

--- regexCoders.py
+++ regexCoders.py
@@ -4,13 +4,19 @@ def captureData(url):
     try:
         respuesta = requests.get(url).json()
         regexExpresion = "^(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}).*?\"\s(\d{3}).*?$"
-        regex = re.findall(respuesta, regexExpresion)
+        regex = re.findall(regexExpresion, respuesta)
         return regex
     except Exception as e:
         return None
 
+def apiRequestData(data):
+    URI = "https://ipinfo.io/"
+    for ip, code in data:
+        print(f"{ip}: {code}")
+
+
 resultado = captureData("https://raw.githubusercontent.com/elastic/examples/refs/heads/master/Common%20Data%20Formats/apache_logs/apache_logs")
-print(resultado)
+apiRequestData(resultado)
 
 
 
