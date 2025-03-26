3ef075dc48220a7e37edb23bb239f4941d182ed6
afmirandadiaz <amirandadiaz@godaddy.com>
2025-03-26 08:09:53 -0500

    New file

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+from regexCoders import *
 
 if __name__ == '__main__':
-    print("Hola mundo")
+    # Ejecutar funciones de captura
+    resultado = captureData(
+        "https://raw.githubusercontent.com/elastic/examples/refs/heads/master/Common%20Data%20Formats/apache_logs/apache_logs")
+    apiRequestData(resultado)
--- regexCoders.py
+++ regexCoders.py
@@ -47,7 +47,3 @@ def apiRequestData(data):
         print(item)
 
     return JsonData
-
-# Ejecutar funciones de captura
-resultado = captureData("https://raw.githubusercontent.com/elastic/examples/refs/heads/master/Common%20Data%20Formats/apache_logs/apache_logs")
-apiRequestData(resultado)
