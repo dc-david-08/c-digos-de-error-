a8445738952f39cae34a7e6a383626be798a8e1f
afmirandadiaz <amirandadiaz@godaddy.com>
2025-03-26 08:23:41 -0500

    New file

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from regexCoders import *
+from AttackDetector import *
 
 if __name__ == '__main__':
-    # Ejecutar funciones de captura
     resultado = captureData(
         "https://raw.githubusercontent.com/elastic/examples/refs/heads/master/Common%20Data%20Formats/apache_logs/apache_logs")
-    apiRequestData(resultado)
+    respuesta = apiRequestData(resultado)
+    russiaAttackDetector(respuesta)
+
