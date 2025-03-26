21b6cbe019da0b200533df87d4df45e232673db4
afmirandadiaz <amirandadiaz@godaddy.com>
2025-03-26 08:28:17 -0500

    New file

--- AttackDetector.py
+++ AttackDetector.py
@@ -1,5 +1,5 @@
 ## Si el pais es Rusia, y el codigo es 200: es un ataque
 def russiaAttackDetector(data):
     for item in data:
-        if item["code"] == 200 and item["Country"] == "Russia":
+        if item["code"] == '200' and item["country"] == "Russia":
             print(f"Se ha presentado un ataque desde {item['Country']}: {item['code']}")
